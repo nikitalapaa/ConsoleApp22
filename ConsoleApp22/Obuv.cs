19471367db18533130e81385e7e9072741ba4284
Лапенко Никита Владимирович <320213@college.local>
2023-03-20 16:34:01 +0300

    Commit

---
+++ ConsoleApp22/Obuv.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp22
+{
+    public class Obuv
+    {
+        public Obuv(string proiz, float cen, double raz)
+        {
+            Proiz = proiz;
+            Cen = cen;
+            Raz = raz;
+        }
+        public string Proiz { get; set; }
+        public float Cen { get; set; }
+        public double Raz { get; set; }
+    }
+}
---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace ConsoleApp22
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Введите количество обуви: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            ArrayList arrayList = new ArrayList(N);
+
+            for (int i = 0; i < N;)
+            {
+                try
+                {
+                    Console.Write("производитель: ");
+                    string proiz = Console.ReadLine();
+
+                    Console.Write("цена: ");
+                    float cen = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("размер: ");
+                    double raz = Convert.ToDouble(Console.ReadLine());
+
+                    Obuv furniture = new Obuv(proiz, cen, raz);
+                    arrayList.Add(furniture);
+
+                    i++;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Введенные данные были неверными");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
